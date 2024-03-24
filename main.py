77c5da5504f47875c84bb59e56f820e7dc246804
FenrirTheLast <147754655+FenrirTheLast@users.noreply.github.com>
2024-03-24 10:40:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.array([0, 1])
+print(a)
+
+a = np.arange(2)
+print(a)
+print(type(a))
+
+print(a.dtype)
+
+a = np.arange(2, dtype ='int32')
+print(a.dtype)
+b = a.astype('float')
+print(b)
